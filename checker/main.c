fa6a249bfd16f6d70a52f94b46b1b7ebdb9ac638
Kochetkov Nickolai <vlegros@21-SCHOOL.RU>
2019-07-01 20:31:34 +0300

    wrote separate methods for pushswap and checker, in checker methods visualisation is happening, have some issues with rearranging val vector

--- checker/main.c
+++ checker/main.c
@@ -12,38 +12,42 @@
 
 #include "push_swap.h"
 
+
 void	del(void **elem)
 {
-	t_vis **temp;
-
-	temp = (t_vis**)elem;
-	ft_memdel(&((*temp)->img));
 	ft_memdel(elem);
 }
 
 int		main(int argc, char **argv)
 {
-	t_ivec *a;
-	t_ivec *b;
-	t_mlx	mlx;
+	t_data dt_a;
+	t_data dt_b;
+	t_mlx mlx;
 
 	if (argc > 1)
 	{
 		check_integers(argc, argv);
-		a = create_stack(argc, argv);
-		if (!(b = ft_int_vec_init()))
+		dt_a.val_vec = create_stack(argc, argv);
+		if (!(dt_b.val_vec = ft_int_vec_init()))
 		{
-			ft_int_vec_del(&a);
+			ft_int_vec_del(&dt_a.val_vec);
 			finish_him(MALLOC_ERROR);
 		}
 		mlx.mlx_ptr = mlx_init();
 		mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 1000, 1000, "chlen");
-		create_elem(&mlx, 15);
+		dt_a.img_vec = vis_vec(&mlx, dt_a.val_vec);
+		dt_b.img_vec = ft_ptr_vec_init();
+		dt_a.stack = 'a';
+		dt_b.stack = 'b';
+		display_stack(&mlx, dt_a.img_vec, dt_a.stack);
+		display_stack(&mlx, dt_b.img_vec, dt_b.stack);
+		//get_input(&dt_a, &dt_b, &mlx);
 		mlx_loop(mlx.mlx_ptr);
-		get_input(&a, &b);
-		check_order(a, b, FINAL) ? ft_printf("OK\n") : ft_printf("KO\n");
-		ft_int_vec_del(&a);
-		ft_int_vec_del(&b);
+		check_order(dt_a.val_vec, dt_b.val_vec, FINAL) ? ft_printf("OK\n") : ft_printf("KO\n");
+		ft_int_vec_del(&dt_a.val_vec);
+		ft_int_vec_del(&dt_b.val_vec);
+		ft_ptr_vec_del(&dt_a.img_vec, del);
+		ft_ptr_vec_del(&dt_a.img_vec, del);
 	}
 	return (0);
 }
