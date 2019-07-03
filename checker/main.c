b3f8fe93a28b2b569c6ef63e1f8ebea6cecd7196
Kochetkov Nickolai <vlegros@21-SCHOOL.RU>
2019-07-03 21:24:10 +0300

    added flags -v and -d for debug and visualisation

--- checker/main.c
+++ checker/main.c
@@ -12,16 +12,46 @@
 
 #include "push_swap.h"
 
-int		main(int argc, char **argv)
+void	hooks(t_manager **checker)
+{
+	mlx_hook((*checker)->win_ptr, 17, 0, del_manager, checker);
+	mlx_loop_hook((*checker)->mlx_ptr, visualize, checker);
+	mlx_loop((*checker)->mlx_ptr);
+}
+
+t_manager	*init(int argc, char **argv)
 {
 	t_manager *checker;
-	if (argc > 1)
+
+	if (!(checker = (t_manager*)malloc(sizeof(t_manager))))
+		finish_him(MALLOC_ERROR);
+	checker->debug = FALSE;
+	checker->visual = FALSE;
+	checker->error = -1;
+	checker->start = 1;
+	checker->start = check_options(&checker, argc, argv);
+	check_integers(checker->start, argc, argv);
+	checker = manager_init(checker, argc, argv);
+	return (checker);
+}
+
+int		main(int argc, char **argv)
+{
+	t_manager	*checker;
+
+	if (argc <= 1)
+		return (0);
+	checker = init(argc, argv);
+	if (checker->visual)
 	{
-		check_integers(argc, argv);
-		checker = manager_init(argc, argv);
-		mlx_loop_hook(checker->mlx_ptr, visualize, checker);
-		mlx_loop(checker->mlx_ptr);
-	/*	check_order(dt_a.val_vec, dt_b.val_vec, FINAL) ? ft_printf("OK\n") : ft_printf("KO\n");*/
+		display_stack(checker, checker->a->img_vec, checker->a->stack);
+		hooks(&checker);
+	}
+	else
+	{
+		get_input(&checker);
+		check_order(checker->a->val_vec, checker->b->val_vec, FINAL) ?
+		ft_printf("OK\n") : ft_printf("KO\n");
 	}
 	return (0);
 }
